5b79f8593abe5f4b765d6749dc419c653400e51d
donp <donp@avelap.(none)>
2011-10-06 00:29:17 -0400

    Added avatar. Need to fix contact form

--- app/controllers/vcard_controller.rb
+++ app/controllers/vcard_controller.rb
@@ -4,4 +4,21 @@ class VcardController < ApplicationController
     @title = "Don Pottinger vBusiness Card"
     @name = "Don Pottinger"
   end
+  
+  def contact
+#$name = stripslashes($_POST['name']);
+#$email = trim($_POST['email']);
+#$subject ="Contact Form Submission";
+#$message = htmlspecialchars($_POST['message']);
+
+  @name = params[:name]
+  @email = params[:email]    
+  @subject = "Contact Form Submission"
+  @message = params[:email]
+
+  respond_to do |format|
+    format.js { render :msg => "OK" }
+  end
+  
+  end
 end
