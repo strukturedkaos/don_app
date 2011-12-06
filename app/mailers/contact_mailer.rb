6b8a44096bbf2da8b87439132237a5bf3b319de9
Don Pottinger <don.pottinger@gmail.com>
2011-12-05 23:03:52 -0500

    Revert c1fa1931b4a07c53bb64eeb5bdb16bf946d87347^..HEAD

--- app/mailers/contact_mailer.rb
+++
@@ -1,8 +0,0 @@
-class ContactMailer < ActionMailer::Base
-  default :to => "don.pottinger@gmail.com"
-  
-  def contact_email(contact)
-    @contact = contact
-    mail(:from => @contact.email, :subject => "Contact Form Submission")
-  end
-end
--- config/initializers/setup_mail.rb
+++
@@ -1,9 +0,0 @@
-ActionMailer::Base.smtp_settings = {  
-  :address              => "smtp.gmail.com",  
-  :port                 => 587,  
-  :domain               => "donapottinger.com",  
-  :user_name            => "don.pottinger",  
-  :password             => "Ygsnohomo2",  
-  :authentication       => "plain",  
-  :enable_starttls_auto => true  
-}  
